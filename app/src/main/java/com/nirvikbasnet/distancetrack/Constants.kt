7808440e55b8fa6e15cbb6c0b545379bc7963a28
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2023-10-11 11:28:47 +1100

    Permission Screen done

---
+++ app/src/main/java/com/nirvikbasnet/distancetrack/Constants.kt
@@ -0,0 +1,5 @@
+package com.nirvikbasnet.distancetrack
+
+object Constants {
+    const val PERMISSION_LOCATION_REQUEST_CODE = 1
+}
---
+++ app/src/main/java/com/nirvikbasnet/distancetrack/Permissions.kt
@@ -0,0 +1,23 @@
+package com.nirvikbasnet.distancetrack
+
+import android.Manifest
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.nirvikbasnet.distancetrack.Constants.PERMISSION_LOCATION_REQUEST_CODE
+import com.vmadalin.easypermissions.EasyPermissions
+
+object Permissions {
+
+    fun hasLocationPermission(context: Context)=
+        EasyPermissions.hasPermissions(context,
+        Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+    fun requestLocationPermission(fragment : Fragment){
+        EasyPermissions.requestPermissions(fragment,
+        "This application cannot work without location permissions",
+            PERMISSION_LOCATION_REQUEST_CODE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+            )
+    }
+}
